25e7cd2a2d3e813b00bd1a9b2d63ae75c54bc366
pnc19121983 <147700726+pnc19121983@users.noreply.github.com>
2025-07-24 12:20:25 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -48,6 +48,7 @@ if selected == "dungchung":
 - [Video bài giảng “Làm chủ AI - Trở thành Super Teacher” do Đại học Bách khoa Hà Nội và Công ty TNHH STEAM for Vietnam đồng tổ chức](https://www.youtube.com/watch?v=5l4Uis5xzvc)                 
 - [Ứng dụng AI trong tạo ảnh và video bài giảng](https://www.youtube.com/watch?v=uQ6URlXLGQA&list=PLKJ7b9uOx27YrBfRzC-Wz6GpvVmZlI-hz&index=4) 
 - [Ứng dụng AI trong giảng dạy Đại học RMIT](https://www.youtube.com/watch?v=4f0kIVRZVn0&t=4950s) 
+- [Ứng dụng AI trong thiết kế bài giảng STEM](https://www.youtube.com/watch?v=R53xuJG5xkk) 
 """)
 
 
